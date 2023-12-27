1e7568041357d424f13e7a3cf5019a2cadb32397
swag2198 <swagatam.haldar9@gmail.com>
2023-12-27 23:38:09 +0100

    combine.sh and run.sh

---
+++ data/combine.sh
@@ -0,0 +1,3 @@
+cp -r CombinedDatasets CombinedDatasetsChallenge
+cp compressedversionv2/images/train/* CombinedDatasetsChallenge/images/train/
+cp compressedversionv2/labels/train/* CombinedDatasetsChallenge/labels/train/
