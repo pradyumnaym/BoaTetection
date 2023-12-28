2e349943a2b9eb399d0f50ac72e8d4393b8d6468
swag2198 <swagatam.haldar9@gmail.com>
2023-12-28 11:59:11 +0100

    lr and nclasses changes

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=DL23
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=16
 #SBATCH --partition=week
-#SBATCH --mem-per-cpu=12G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --gres=gpu:A4000:4
 #SBATCH --time=3-23:0:0
 #SBATCH --output=runs/job.%J.out
 #SBATCH --mail-type=ALL
 
-WANDB_API_KEY=a3e66938bb695d3dea4bd75b92b5abe155c7759a 
-singularity exec --nv  /common/singularityImages/DL23.sif python -m torch.distributed.launch --nproc_per_node 4 --master_port 9527 train.py --workers 8 --device 0,1,2,3 --sync-bn --batch-size 64 --data data/BoaTetection.yaml --img 640 640 --cfg cfg/training/yolov7x.yaml --weights 'yolov7x.pt' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+WANDB_MODE=disabled
+singularity exec --nv  /common/singularityImages/DL23.sif python -m torch.distributed.launch --nproc_per_node 4 --master_port 9527 train.py --workers 16 --device 0,1,2,3 --sync-bn --batch-size 64 --data data/BoaTetection.yaml --img 640 640 --cfg cfg/training/yolov7x.yaml --weights 'yolov7x.pt' --name yolov7 --hyp data/hyp.scratch.p5.yaml
 echo DONE!
