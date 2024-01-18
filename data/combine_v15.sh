858cf795515327311465259a51408ae40326c224
swag2198 <swagatam.haldar9@gmail.com>
2024-01-18 20:52:28 +0100

    combine v15

---
+++ data/combine_v15.sh
@@ -0,0 +1,7 @@
+cp -r CombinedDatasets CombinedDatasetsChallenge_v15
+cp compressedversionv2/images/train/* CombinedDatasetsChallenge_v15/images/train/
+cp compressedversionv2/labels/train/* CombinedDatasetsChallenge_v15/labels/train/
+
+cp SibenikDataset/images/train/* CombinedDatasetsChallenge_v15/images/train/
+cp SibenikDataset/labels/train/* CombinedDatasetsChallenge_v15/labels/train/
+
